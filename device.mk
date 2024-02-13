2e1fff7c87201f4737dac683cb47536653318508
Anay Wadhera <anay1018@gmail.com>
2024-02-13 21:26:51 +0530

    sky: move board flag to device.mk
    Change-Id: I7802618c684cd853d99c2f0ad87b82eab9cb31da

--- device.mk
+++ device.mk
@@ -5,6 +5,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Board
+TARGET_BOARD_PLATFORM := parrot
+
 # A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/android_t_baseline.mk)
