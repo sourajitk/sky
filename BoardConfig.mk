b9583caf4edb11d00cc6706a76578698db80b21e
Anay Wadhera <anay1018@gmail.com>
2024-02-24 17:39:14 -0500

    sky: Permissive

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -72,7 +72,8 @@ BOARD_KERNEL_CMDLINE := video=vfb:640x400,bpp=32,memsize=3072000
 BOARD_BOOTCONFIG := \
     androidboot.hardware=qcom \
     androidboot.memcg=1 \
-    androidboot.usbcontroller=a600000.dwc3
+    androidboot.usbcontroller=a600000.dwc3 \
+    androidboot.selinux=permissive
 
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_BOOT_HEADER_VERSION := 4
